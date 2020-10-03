675cdbded0f86544e7f6e478a644ba8b8cee3d00
Steven Waldron <steven.waldron@pm.me>
2020-10-03 18:16:54 -0500

    Test out image lib to get dimensions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+pub struct Cmd {
+    #[structopt(parse(from_os_str))]
+    pub image: PathBuf,
+}
+
+fn main() -> Result<(), image::ImageError> {
+    let opts = Cmd::from_args();
+    let (x, y) = image::image_dimensions(opts.image)?;
+
+    println!("Successfully loaded image!");
+    println!("Image size {} x {}", x, y);
+
+    Ok(())
 }
